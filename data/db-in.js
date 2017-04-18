87de0cd64b8af5aa4a8cdb4ef57aab51bdab312b
Adam Silverstein <adam@10up.com>
2017-04-17 21:36:13 -0400

    Adjust data dates

--- data/db-in.js
+++ data/db-in.js
@@ -1,156 +1,156 @@
 module.exports.wordpressVersions = [
 	{
-		'released': '?',
+		'released': '',
 		'version':  '0.71',
 		'folder':   '071'
 	},
 	{
-		'released': 'January 3, 2004',
+		'released': 'January, 2004',
 		'version':  '1.0',
 		'folder':   '10'
 	},
 	{
-		'released': 'May 22, 2004',
+		'released': 'May, 2004',
 		'version':  '1.2',
 		'folder':   '12'
 	},
 	{
-		'released': 'February 17, 2005',
+		'released': 'February, 2005',
 		'version':  '1.5',
 		'folder':   '15'
 	},
 	{
-		'released': 'December 26, 2005',
+		'released': 'December, 2005',
 		'version':  '2.0',
 		'folder':   '20'
 	},
 	{
-		'released': 'January 22, 2007',
+		'released': 'January, 2007',
 		'version':  '2.1',
 		'folder':   '21'
 	},
 	{
-		'released': 'May 16, 2007',
+		'released': 'May, 2007',
 		'version':  '2.2',
 		'folder':   '22'
 	},
 	{
-		'released': 'September 24, 2007',
+		'released': 'September, 2007',
 		'version':  '2.3',
 		'folder':   '23'
 	},
 	{
-		'released': 'March 29, 2008',
+		'released': 'March, 2008',
 		'version':  '2.5',
 		'folder':   '25'
 	},
 	{
-		'released': 'July 15, 2008',
+		'released': 'July, 2008',
 		'version':  '2.6',
 		'folder':   '26'
 	},
 	{
-		'released': 'December 10, 2008',
+		'released': 'December, 2008',
 		'version':  '2.7',
 		'folder':   '27'
 	},
 	{
-		'released': 'June 10, 2009',
+		'released': 'June, 2009',
 		'version':  '2.8',
 		'folder':   '28'
 	},
 	{
-		'released': 'December 18, 2009',
+		'released': 'December, 2009',
 		'version':  '2.9',
 		'folder':   '29'
 	},
 	{
-		'released': 'June 17, 2010',
+		'released': 'June, 2010',
 		'version':  '3.0',
 		'folder':   '30'
 	},
 	{
-		'released': 'February 23, 2011',
+		'released': 'February, 2011',
 		'version':  '3.1',
 		'folder':   '31'
 	},
 	{
-		'released': 'July 4, 2011',
+		'released': 'July, 2011',
 		'version':  '3.2',
 		'folder':   '32'
 	},
 	{
-		'released': 'December 12, 2011',
+		'released': 'December, 2011',
 		'version':  '3.3',
 		'folder':   '33'
 	},
 	{
-		'released': 'June 13, 2012',
+		'released': 'June, 2012',
 		'version':  '3.4',
 		'folder':   '34'
 	},
 	{
-		'released': 'December 11, 2012',
+		'released': 'December, 2012',
 		'version':  '3.5',
 		'folder':   '35'
 	},
 	{
-		'released': 'August 1, 2013',
+		'released': 'August, 2013',
 		'version':  '3.6',
 		'folder':   '36'
 	},
 	{
-		'released': 'October 24, 2013',
+		'released': 'October, 2013',
 		'version':  '3.7',
 		'folder':   '37'
 	},
 	{
-		'released': 'December 12, 2013',
+		'released': 'December, 2013',
 		'version':  '3.8',
 		'folder':   '38'
 	},
 	{
-		'released': 'April 16, 2014',
+		'released': 'April, 2014',
 		'version':  '3.9',
 		'folder':   '39'
 	},
 	{
-		'released': 'September 4, 2014',
+		'released': 'September, 2014',
 		'version':  '4.0',
 		'folder':   '40'
 	},
 	{
-		'released': 'December 17, 2014',
+		'released': 'December, 2014',
 		'version':  '4.1',
 		'folder':   '41'
 	},
 	{
-		'released': 'April 23, 2015',
+		'released': 'April, 2015',
 		'version':  '4.2',
 		'folder':   '42'
 	},
 	{
-		'released': 'August 18, 2015',
+		'released': 'August, 2015',
 		'version':  '4.3',
 		'folder':   '43'
 	},
 	{
-		'released': 'December 8, 2015',
+		'released': 'December, 2015',
 		'version':  '4.4',
 		'folder':   '44'
 	},
 	{
-		'released': 'April 12, 2016',
+		'released': 'April, 2016',
 		'version':  '4.5',
 		'folder':   '45'
 	},
 	{
-		'released': 'August 16, 2016',
+		'released': 'August, 2016',
 		'version':  '4.6',
 		'folder':   '46'
 	},
 	{
-		'released': 'December 6, 2016',
+		'released': 'December, 2016',
 		'version':  '4.7',
 		'folder':   '47'
 	}
